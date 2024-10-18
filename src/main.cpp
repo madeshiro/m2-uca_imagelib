117ca3321b80ccf7989fa7356a5001e3728fa1f1
redclems <clement.guerin27@free.fr>
2024-10-19 00:09:27 +0200

    make CSV at the end of traitemnt 'img011.pn make segmentation fault I doen't no why ?'

--- src/main.cpp
+++ src/main.cpp
@@ -13,103 +13,53 @@
 #include <string>
 #include <ctime>  // To generate the current date and time
 #include <sstream> // For formatting the filename
-/**
- * @struct ImageData
- * Struct to store the data for each image tested
-*/
-struct ImageData {
-    std::string imageName;
-    cv::Point laserIntersection;
-    std::vector<cv::Vec2d> plantPositions;
-    std::vector<cv::Vec2d> advantisPositions;
-    LaserBehavior laserOnObject; 
-};
 
-/**Get the current date and time as a formatted string
- * The date and time format is: YYYY-MM-DD_HH-MM-SS
- * @return string representing date and time
- */
-std::string getCurrentDateTime() {
-    time_t now = time(0);
-    struct tm tstruct;
-    char buf[80];
-    tstruct = *localtime(&now);
-    strftime(buf, sizeof(buf), "%Y-%m-%d_%H-%M-%S", &tstruct); //YYYY-MM-DD_HH-MM-SS
-    return std::string(buf);
-}
-
-/**
- * Format a vector of 2D positions into a string
- * like "(x1; y1)/ (x2; y2)/"
- * @param vector of 2D points (cv::Vec2d)
- * @return string representing 2D positions
- */
-std::string formatPositions(const std::vector<cv::Vec2d>& positions) {
-    std::stringstream ss;
-    for (const auto& pos : positions) {
-        ss << "(" << pos[0] << "; " << pos[1] << ")/ ";
-    }
-    std::string result = ss.str();
-    if (!result.empty()) {
-        result.pop_back();  // Remove last space
-        result.pop_back();  // Remove last comma
-    }
-    return result;
-}
-
-/**
- * Generates and writes data to a CSV file with the name format
- * "YYYY-MM-DD_HH-MM-SS_plantCheck.csv". It records the image name, laser intersection point,
- * laser state, plant positions, and advantis positions.
- * @param imageDataList A vector of ImageData structs containing data for each image.
- */
-void generateCSV(const std::vector<ImageData>& imageDataList) {
-
-    //The name of file
-    std::string filename = getCurrentDateTime() + "_plantCheck.csv";
-
-    std::ofstream csvFile;
-    csvFile.open(filename, std::ios::app);
-    if (!csvFile.is_open()) {
-        std::cerr << "Error: Unable to open or create the CSV file." << std::endl;
-        return;
+     /**Get the current date and time as a formatted string
+     * The date and time format is: YYYY-MM-DD_HH-MM-SS
+     * @return string representing date and time
+     */
+    std::string getCurrentDateTime() 
+    {
+        time_t now = time(0);
+        struct tm tstruct;
+        char buf[80];
+        tstruct = *localtime(&now);
+        strftime(buf, sizeof(buf), "%Y-%m-%d_%H-%M-%S", &tstruct); //YYYY-MM-DD_HH-MM-SS
+        return std::string(buf);
     }
 
-    // Write the header
-    csvFile << "Image Name, Laser Intersection (X; Y), LaserOn, Advantis Positions (X; Y), Weed Positions\n";
+    /**
+     * Generates and writes data to a CSV file with the name format
+     * "YYYY-MM-DD_HH-MM-SS_plantCheck.csv". It records the image name, laser intersection point,
+     * laser state, plant positions, and advantis positions
+     * @param factory containing data for each image
+     */
 
-    // Write the data for each image
-    for (const auto& data : imageDataList) {
-        std::string plantPosStr = formatPositions(data.plantPositions);
-        std::string weedPosStr = formatPositions(data.advantisPositions);
+    void generateCSV(const idl::ProcessingFactory& factory) {
+        //The name of file
+        std::string filename = getCurrentDateTime() + "_plantCheck.csv";
 
-        //type of beahvior the laser 
-        std::string laserOnStr = "Unknown";
-        switch (data.laserOnObject) {
-            case LaserBehavior::onNothing:
-                laserOnStr = "onNothing";
-                break;
-            case LaserBehavior::onAdventis:
-                laserOnStr = "onWheat";
-                break;
-            case LaserBehavior::onWheat:
-                laserOnStr = "onAdventis";
-                break;
-            default:
-                laserOnStr = "Unknown";
-                break;
+        std::ofstream csvFile;
+        csvFile.open(filename, std::ios::app);
+        if (!csvFile.is_open()) {
+            std::cerr << "Error: Unable to open or create the CSV file." << std::endl;
+            return;
         }
 
-        csvFile << data.imageName << ", ("
-                << data.laserIntersection.x << "; " << data.laserIntersection.y << "), "
-                << laserOnStr << ", "
-                << "\"" << plantPosStr << "\"" << ", "
-                << "\"" << weedPosStr << "\"\n";
+        // Write the header
+        csvFile << "Image Name, Laser Intersection (X; Y), LaserOn, Advantis Positions (X; Y), Weed Positions\n";
+
+        //write all data
+        for(const auto& ImageProc : factory.listProcessing())
+        {
+            ImageProc.write(csvFile);
+        }
+
+        csvFile.close();
+        std::cout << "CSV file '" << filename << "' successfully created and updated!" << std::endl;
+        
     }
 
-    csvFile.close();
-    std::cout << "CSV file '" << filename << "' successfully created and updated!" << std::endl;
-}
 
 int main()
 {
@@ -120,7 +70,8 @@ int main()
 
     int i = 0;
     while(1)
-    {    
+    {   
+         
         cv::Mat imgs[2] = {
             factory[i].getImageWithDetails(),
             factory[i].getImageWithMasks()
@@ -135,34 +86,17 @@ int main()
                 return 0;
             }
         }
-
+    
         i = (++i%factory.listProcessing().size());
         if(0 == i)
         {
+            generateCSV(factory);
             return 0;
         }
+
     }
 
     return 0;
 }
 
-/*
-
-        // Detect the laser intersection
-        //idl::LineDetector lineDetector(image2);
-        cv::Point laserIntersection(204, 208);
-       	// Get image name
-        std::string imageName = dataFileNames[i].substr(dataFileNames[i].find_last_of("/\\") + 1);
-        laserBehavior imagelaserOn = laserBehavior::onWheat;
-
-        // Store the data for CSV
-        ImageData imageData;
-        imageData.imageName = imageName;
-        imageData.laserIntersection = laserIntersection;
-        imageData.plantPositions = plantPositions;
-        imageData.advantisPositions = advantisPositions;
-        imageData.laserOnObject = imagelaserOn; 
-        imageDataList.push_back(imageData);
-        generateCSV(imageDataList);
-*/
 
